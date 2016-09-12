d73320aa2866dfa01844a08e06650a4ecc2cca19
aliesp1712 <alina.espinoza@ug.uchile.cl>
2016-09-12 17:26:43 -0300

    nuevo titulo Ali

--- ejemplo_seno.py
+++ ejemplo_seno.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0,15)
-plt.plot(x, np.sin(x), 'o')
+plt.plot(x, np.sin(x), '-o')
 plt.savefig("Seno")
+plt.title('ali')
 plt.show()
